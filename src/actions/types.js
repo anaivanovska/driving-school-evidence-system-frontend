7724821d5d65370876e0abb5ca7224c43015ffc5
anaivanovska <annaivanovskaa@gmail.com>
2019-09-25 18:20:34 +0200

    Initial commit

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+export const LOGIN_ACTION = "login";
+export const SIGNIN_ACTION = "signin";
+export const FORGOT_PASSWORD_ACTION = "forgot password";
+export const FIND_USER_BY_USERNAME = "find user by username";
